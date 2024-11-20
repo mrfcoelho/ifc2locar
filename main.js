df386bc528cd0765e92adad63a0030a558e18f6a
mrfcoelho <mrfcoelho@gmail.com>
2024-11-20 09:01:06 +0000

    [FEAT] update gps coords

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ import * as WEBIFC from "web-ifc";
 
 // setupCounter(document.querySelector("#counter"));
 
-// hello world
+// setuo the scene
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   60,
@@ -45,6 +45,8 @@ window.addEventListener("resize", (e) => {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
 });
+
+// setup the models to be shown in the scene
 const box = new THREE.BoxGeometry(20, 20, 20);
 const cube = new THREE.Mesh(
   box,
@@ -56,19 +58,86 @@ const locar = new LocAR.LocationBased(scene, camera);
 //responsible for rendering the camera feed
 const cam = new LocAR.WebcamRenderer(renderer);
 
+//--------------------- example with fake gps and cube ---------------------//
 /**
  * fakeGps(lon, lat, elev=null, acc=0) : fakes a GPS position being received. Elevation and accuracy can optionally be provided.
  * To place ourselves (i.e. the camera) at a given real-world location.
  * lon: longitude, distance to Greenwich
  * lat: latitude, distance to equator
  */
-//locar.fakeGps(-8.4053, 41.5466);
+//locar.fakeGps(-8.40, 41.457); //H
+//locar.fakeGps(-8.29, 41.45); //O
 // add 3D model to a specific real-world location defined by longitude and latitude
-//locar.add(cube, -8.4053, 41.546601);
-locar.add(cube, -8.410888279369487, 41.538707419973);
+//locar.add(cube, -8.40, 41.548); //H
+//locar.add(cube, -8.29, 41.451); //O
+
+//--------------------- example with fake gps and cube ---------------------//
+//--------------------- example with real gps and cube ---------------------//
+
+let firstLocation = true;
+
+const deviceOrientationControls = new LocAR.DeviceOrientationControls(camera);
+
+locar.on("gpsupdate", (pos, distMoved) => {
+  if (firstLocation) {
+    alert(
+      `Got the initial location: longitude ${pos.coords.longitude}, latitude ${pos.coords.latitude}`
+    );
+
+    const boxProps = [
+      {
+        latDis: 0.001,
+        lonDis: 0,
+        colour: 0xff0000,
+      },
+      {
+        latDis: -0.001,
+        lonDis: 0,
+        colour: 0xffff00,
+      },
+      {
+        latDis: 0,
+        lonDis: -0.001,
+        colour: 0x00ffff,
+      },
+      {
+        latDis: 0,
+        lonDis: 0.001,
+        colour: 0x00ff00,
+      },
+    ];
+
+    const geom = new THREE.BoxGeometry(20, 20, 20);
+
+    for (const boxProp of boxProps) {
+      const mesh = new THREE.Mesh(
+        geom,
+        new THREE.MeshBasicMaterial({ color: boxProp.colour })
+      );
+
+      console.log(
+        `adding at ${pos.coords.longitude + boxProp.lonDis},${
+          pos.coords.latitude + boxProp.latDis
+        }`
+      );
+      locar.add(
+        mesh,
+        pos.coords.longitude + boxProp.lonDis,
+        pos.coords.latitude + boxProp.latDis
+      );
+    }
+
+    firstLocation = false;
+  }
+});
+
+locar.startGps();
+
+//--------------------- example with real gps and cube ---------------------//
 
 renderer.setAnimationLoop(animate);
 
+// function that will update the came
 function animate() {
   cam.update();
   renderer.render(scene, camera);
